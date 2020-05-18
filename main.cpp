ba7f76720fd8084c7fd3947110128f1ae895e3d3
Carolina Nunes Pinheiro <nunespcarolina@gmail.com>
2020-05-18 19:04:22 -0300

    Consolidado de Renda

--- main.cpp
+++ main.cpp
@@ -3,7 +3,7 @@
 using namespace std;
 
 int main() {
-	double  sal, ser, cap, med, edu;
+	double  sal, ser, cap, med, edu, salmensal, isal, iser, icap;
 	
 	cout << fixed << setprecision (2.0);
 	
@@ -17,6 +17,27 @@ cout << "Gastos medicos: ";
 cin >> med;
 cout << "Gastos educacionais: ";
 cin >> edu;
+
+salmensal = sal/12;
+
+if (salmensal < 3000) {
+	isal == 0.00;
+	}
+else if (salmensal >= 3000 && salmensal < 5000) {
+	isal = sal * 0.1;
+}
+else {
+	isal = sal * 0.2;
+}
+iser = ser * 0.15;
+icap = cap * 0.20;
+
+cout << "Relatorio de imposto de renda" << endl ; 
+cout << "Cosolidado de renda :" << endl ;
+cout << "Imposto sobre salario:" << isal << endl;
+cout << "Imposto sobre servicos:" << iser << endl;
+cout << "Imposto sobre ganho de capital:" << icap<< endl;
+
 
 	return 0;
 }
